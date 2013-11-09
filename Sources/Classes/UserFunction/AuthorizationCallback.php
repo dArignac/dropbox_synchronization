29f3a6ded6b0abe49ee21163eff551581e47fa2c
Alexander Herrmann <darignac@googlemail.com>
2013-11-09 15:12:52 +0100

    extension sceleton created with extension_kickstarter

---
+++ Sources/Classes/UserFunction/AuthorizationCallback.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package dropbox_synchronization
+ */
+class Tx_DropboxSynchronization_UserFunction_AuthorizationCallback {
+
+    public function respond($content, $configuration) {
+        echo "222";
+    }
+}
---
+++ Sources/ext_autoload.php
@@ -0,0 +1,5 @@
+<?php
+$extensionClassesPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('dropbox_synchronization') . 'Classes/';
+return array(
+    'Tx_DropboxSynchronization_UserFunction_AuthorizationCallback' => $extensionClassesPath . 'UserFunction/AuthorizationCallback.php',
+);
