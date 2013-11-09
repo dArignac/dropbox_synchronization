c03fbaa490809b96c19f51653e4d0d71d7ac3b11
Alexander Herrmann <darignac@googlemail.com>
2013-11-09 15:23:53 +0100

    finally, something works here

--- Sources/Classes/UserFunction/AuthorizationCallback.php
+++ Sources/Classes/UserFunction/AuthorizationCallback.php
@@ -4,7 +4,15 @@
  */
 class Tx_DropboxSynchronization_UserFunction_AuthorizationCallback {
 
+    /**
+     * @var Tx_DropboxSynchronization_Service_DropboxService
+     */
+    public $dropboxService;
+
     public function respond($content, $configuration) {
-        echo "222";
+        if ($this->dropboxService == null) {
+            $this->dropboxService = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Tx_DropboxSynchronization_Service_DropboxService');
+        }
+        echo "Tx_DropboxSynchronization_UserFunction_AuthorizationCallback";
     }
 }
